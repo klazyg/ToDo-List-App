fac72f8b5601e484a88e7071a5b06e2670f3cf9a
klazyg <kzygmunt0@gmail.com>
2022-11-09 12:13:51 +0100

    Create a form

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Button.module.scss";
 
 function Button() {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+  const nameRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setInput("");
+    setName("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.taskForm}>
+      <>
+        <input
+          type="name"
+          placeholder="Your Name"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          className={styles.taskForm__name}
+          ref={nameRef}
+        />
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          className={styles.taskForm__task}
+          ref={inputRef}
+        />
+        <button onClick={handleSubmit} className={styles.taskForm__btn}>
+          Add Task
+        </button>
+      </>
+    </form>
+  );
 }
 
 export default Button;
