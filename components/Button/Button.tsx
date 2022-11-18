b51c45ac47d283718624308b9fa0f384a8462a83
klazyg <kzygmunt0@gmail.com>
2022-11-18 11:00:19 +0100

    move the form to index

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,42 +1,13 @@
-import React, { useState } from "react";
-import { useForm } from 'react-hook-form';
+import React from "react";
 import styles from "./Button.module.scss";
-import Input from '../Input/Input';
 
-interface ButtonProps {
-  onSubmit: any;
-}
-
-export default function Button(props: ButtonProps) {
-  const { register, handleSubmit } = useForm();
+export default function Button() {
   return (
-    <form className={styles.taskForm} onSubmit={handleSubmit(props.onSubmit)}>
-      <Input
-        placeholder="Name"
-        name="name"
-        formRef={register('name', { required: true })}
-      />
-      <Input
-        placeholder="Add new"
-        name="task"
-        formRef={register('task', { required: true })}
-      />
-      <Input
-        placeholder="Priority"
-        name="priority"
-        formRef={register('priority', { required: true })}
-      />
-      <Input
-        placeholder="Date"
-        name="date"
-        formRef={register('date', { required: true })}
-      />
-      <button
-        className={styles.taskForm__btn}
-        type="submit"
-      >
-        Add Task
-      </button>
-    </form>
+    <button
+      className={styles.btn}
+      type="submit"
+    >
+      Add Task
+    </button>
   );
 }
