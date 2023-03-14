ad77777847cd224407120b0428003e35ee63aedc
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-03-14 18:29:48 +0100

    create: API

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,25 +1,49 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, Prisma } from '@prisma/client';
+import * as fs from "fs";
+import { NextApiRequest } from "next";
 
-const prisma = new PrismaClient();
+const FILENAME = "tasks.json";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+interface Task {
+  id: number;
+  name: string;
+  task: string;
+  priority: string;
+  date: string;
+}
 
-    try {
-        console.log(req.body);
+const saveData = (tasks: Task[]) => {
+  fs.writeFileSync(FILENAME, JSON.stringify({ tasks }));
+};
 
-        const {name, task, priority, date} = JSON.parse(req.body)
-        console.log(name);
-        const savedTask = await prisma.task.create({
-          data: {
-            name,
-            task,
-            deadlineTimestamp: Number(date),
-          },
-        });
-        res.status(200).json(savedTask);
-      } catch (err) {
-        console.log(err);
-        res.status(400).json({ message: 'Something went wrong' });
-      }
+const readData = () => {
+  try {
+    const data = JSON.parse(fs.readFileSync(FILENAME, "utf8")) as {
+      tasks: Task[];
     };
+    return data;
+  } catch (error) {
+    return { tasks: [] };
+  }
+};
+
+export default function handler(req: NextApiRequest, res) {
+  const { tasks } = req.body;
+  const db = readData();
+
+  switch (req.method) {
+    case "POST":
+      db.tasks = [...db.tasks, ...tasks];
+      saveData(db.tasks);
+      return res.status(200).end();
+
+    case "GET":
+      console.log(db);
+      return res.status(200).json(db);
+
+    case "DELETE":
+      const { taskId } = req.body;
+      db.tasks = db.tasks.filter((task) => task.id !== taskId);
+      saveData(db.tasks);
+      return res.status(200).end();
+  }
+}
