414dce4f979032aa65afbd04620d7269e49811d5
Klaudia Zygmunt <kzygmunt0@gmail.com>
2023-03-14 18:27:33 +0100

    build: header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,15 @@
-import React from "react";
 import styles from "./Header.module.scss";
 import { FaEarlybirds } from "react-icons/fa";
 
-function Header() {
+const Header: React.FC = () => {
   return (
-    <div className={styles.logo}>
-      <FaEarlybirds size="3rem" />
-      <h1>XYZ.company</h1>
-    </div>
+    <header className={styles.position}>
+      <div className={styles.logo}>
+        <FaEarlybirds size="3rem" />
+        <h1 className={styles.title}>To-Do List App</h1>
+      </div>
+    </header>
   );
-}
+};
 
 export default Header;
